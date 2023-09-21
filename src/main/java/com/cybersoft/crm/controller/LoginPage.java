a26613d0adbb6d252ae07db5eb2ba563403c7e9e
dhammi19 <daohoanh.work@gmail.com>
2023-09-21 22:05:24 +0700

    import servlet, jstl and mysql library, add html files, config database connection, design pattern and make login function

---
+++ src/main/java/com/cybersoft/crm/controller/LoginPage.java
@@ -0,0 +1,27 @@
+package com.cybersoft.crm.controller;
+
+import com.cybersoft.crm.service.LoginService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "loginPage", urlPatterns = {"/login"})
+public class LoginPage extends HttpServlet {
+    private LoginService loginService = new LoginService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        boolean isLogin = loginService.checkLogin(email, password);
+
+        System.out.println("Kiếm tra login: "+isLogin);
+
+        req.getRequestDispatcher("/webapp/login.html").forward(req, resp);
+    }
+}
