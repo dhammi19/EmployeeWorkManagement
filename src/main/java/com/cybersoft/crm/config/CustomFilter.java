1ede577f53204b841f85ab1a40d89b1cdace4407
dhammi19 <daohoanh.work@gmail.com>
2023-09-22 15:30:46 +0700

    demo cookie, session and filter when login

---
+++ src/main/java/com/cybersoft/crm/config/CustomFilter.java
@@ -0,0 +1,53 @@
+package com.cybersoft.crm.config;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/*"}) // Filter sẽ hoạt động với url chỉ định đó là tất cả các url vì /*
+public class CustomFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("Kiểm tra filter");
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        HttpSession session = request.getSession();
+        if (session.getAttribute("isLogin")!= null && !session.getAttribute("isLogin").equals("")) {
+            boolean isLogin = (boolean) session.getAttribute("isLogin");
+
+            if (isLogin) {
+                // chuyển về page chỉ định
+                if (request.getServletPath().equals("/login")) {
+                    // nếu là trang login
+                    response.sendRedirect(request.getContextPath()+"/home");
+                } else {
+                    filterChain.doFilter(request, response);
+                }
+            } else {
+                // chuyển về page login
+                response.sendRedirect(request.getContextPath()+"/login");
+            }
+        } else {
+            // chuyển về page login
+            System.out.println("Kiếm tra path "+request.getServletPath());
+            if (request.getServletPath().equals("/login")) {
+                filterChain.doFilter(request, response);
+            } else {
+                response.sendRedirect(request.getContextPath()+"/login");
+            }
+
+        }
+
+//        filterChain.doFilter(servletRequest, servletResponse); // Cho phép đi vào link mong muốn
+    }
+}
--- src/main/java/com/cybersoft/crm/controller/LoginPage.java
+++ src/main/java/com/cybersoft/crm/controller/LoginPage.java
@@ -4,9 +4,7 @@ import com.cybersoft.crm.service.LoginService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 
 @WebServlet(name = "loginPage", urlPatterns = {"/login"})
@@ -18,7 +16,26 @@ public class LoginPage extends HttpServlet {
         String email = req.getParameter("email");
         String password = req.getParameter("password");
 
+//        Cookie cookie = new Cookie("email", email); // Khai báo 1 cookie tên là email
+//        cookie.setMaxAge(5*60); // Đặt thời gian coolie là 60s
+//        resp.addCookie(cookie); // Thông báo cho client biết rắng server muốn tạo ra 1 cooke tên là email
+//
+//        Cookie cookie1 = new Cookie("password", password);
+//        cookie1.setMaxAge(5*60);
+//        resp.addCookie(cookie1);
+//
+//        Cookie[] cookies = req.getCookies();
+//        for (Cookie ck : cookies) {
+//            System.out.println("Name cookie: "+ck.getName()+" Value cookie: "+ck.getValue());
+//        }
+
         boolean isLogin = loginService.checkLogin(email, password);
+
+        if (isLogin) {
+            HttpSession httpSession = req.getSession(); // Yêu cầu sử dụng session
+            httpSession.setAttribute("isLogin", true);
+            httpSession.setMaxInactiveInterval(8*60);
+        }
 
         System.out.println("Kiếm tra login: "+isLogin);
 
